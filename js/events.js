f726ae537908513f1ff88831058e6ead25f4edbf
Jonah Chates <jonah.chates@gmail.com>
2019-06-18 20:13:08 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -20,6 +20,13 @@ function pressIt(){
     })
 }
 
+function submitIt(){
+  $("form").on("submit",
+  function() {
+    alert('Your form is going to be submitted now.');})
+  
+}
+
 
 $(document).ready(function(){
   
